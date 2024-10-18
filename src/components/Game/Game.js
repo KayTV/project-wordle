9ba2906a51403623f14a9b8fbc4d3e7a735cabd7
KaylynTV39 <kaylynvannorstrand@navapbc.com>
2024-10-18 12:31:17 -0400

    completed code challenge

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,6 +1,8 @@
+import Guess from '../Guess/Guess';
 import Input from '../Input';
 import React from 'react';
 import { WORDS } from '../../data';
+import { checkGuess } from '../../game-helpers';
 import { sample } from '../../utils';
 
 // Pick a random word on every pageload.
@@ -10,19 +12,64 @@ console.info({ answer });
 
 function Game() {
   const [guess, setGuess] = React.useState([]);
+  const [guessArr, setGuessArr] = React.useState([]);
+  const [winBanner, setWinBanner] = React.useState(false);
+  const [loseBanner, setLoseBanner] = React.useState(false);
 
   function handleGuess(input) {
-    const guessArray = [...guess, input]
-    setGuess(guessArray)
+    if (guess.length <= 5) {
+      const guessArray = [...guess, input]
+      splitGuess(input)
+      setGuess(guessArray)
+    }
+    if (guess.length >= 5 && !winBanner) {
+      setLoseBanner(true);
+    }
+    return;
+  }
+
+  function splitGuess(input) {
+    const guessSplit = checkGuess(input, answer);
+    const newArr = [...guessArr, guessSplit];
+    const lookForCorrectAnswer = guessSplit.filter(value => value.status === 'incorrect' || value.status === 'misplaced');
+
+    if (lookForCorrectAnswer.length === 0) {
+      setWinBanner(true);
+    }
+
+    setGuessArr(newArr);
+  }
+
+  function bannerMessage() {
+    if (winBanner) {
+      return <div class="happy banner">
+              <p>
+                <strong>Congratulations!</strong> Got it in
+                <strong> {guess.length} guesses</strong>.
+              </p>
+            </div>
+    }
+    if (loseBanner) {
+      return <div class="sad banner">
+              <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
+            </div>
+    }
+    return;
   }
 
   return <>
-    <div class="guess-results">
+    {/* <div class="guess-results">
       {guess.map((value) => (
-        <p class="guess">{value}</p>
+        <p class="guess">
+          {value.split('').map((letter)=> (
+            <span class="cell">{letter}</span>
+          ))}
+        </p>
       ))}
-    </div>
+    </div> */}
+    <Guess guessArr={guessArr} />
     <Input handleGuess={handleGuess} />
+    {bannerMessage()}
   </>;
 }
 
---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,34 @@
+import {NUM_OF_GUESSES_ALLOWED} from '../../constants';
+import React from 'react';
+
+function Guess({guessArr}) {
+  function guessFunctionColor(index, placement) {
+    if (guessArr && guessArr[index]) {
+      return guessArr[index][placement].status;
+    }
+    return;
+  }
+
+  function guessFunctionLetter(index, placement) {
+    if (guessArr && guessArr[index]) {
+      return guessArr[index][placement].letter;
+    }
+    return;
+  }
+  console.log(guessArr)
+  return <>
+    <div class="guess-results">
+      {[...Array(NUM_OF_GUESSES_ALLOWED)].map((e, i) => (
+        <p class="guess" key={i}>
+          <span className={"cell " + guessFunctionColor(i, 0)}>{guessFunctionLetter(i, 0)}</span>
+          <span className={"cell " + guessFunctionColor(i, 1)}>{guessFunctionLetter(i, 1)}</span>
+          <span className={"cell " + guessFunctionColor(i, 2)}>{guessFunctionLetter(i, 2)}</span>
+          <span className={"cell " + guessFunctionColor(i, 3)}>{guessFunctionLetter(i, 3)}</span>
+          <span className={"cell " + guessFunctionColor(i, 4)}>{guessFunctionLetter(i, 4)}</span>
+        </p>
+      ))}
+    </div>
+  </>;
+}
+
+export default Guess;
